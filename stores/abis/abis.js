9d2ef3874c02a06db61212894252a0cdaa1b75fa
Aditya Gautam <adityagautam18@gmail.com>
2022-05-22 15:22:56 +0530

    Create abis.js

---
+++ stores/abis/abis.js
@@ -0,0 +1,29 @@
+import { erc20ABI } from './erc20ABI'
+import { veTokenABI } from './veTokenABI'
+import { factoryABI } from './factoryABI'
+import { pairABI } from './pairABI'
+import { routerABI } from './routerABI'
+import { voterABI } from './voterABI'
+import { gaugeABI } from './gaugeABI'
+import { bribeABI } from './bribeABI'
+import { tokenABI } from './tokenABI'
+import { veDistABI } from './veDistABI'
+import { minterABI } from './minterABI'
+import { wftmABI } from './wftmABI'
+import { solidlyLibraryABI } from './solidlyLibraryABI'
+
+export default {
+  erc20ABI,
+  veTokenABI,
+  factoryABI,
+  pairABI,
+  routerABI,
+  voterABI,
+  bribeABI,
+  gaugeABI,
+  veDistABI,
+  tokenABI,
+  minterABI,
+  wftmABI,
+  solidlyLibraryABI
+}
