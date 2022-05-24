121d614bb79fe9231c93e7ab4a73e6745db95ea3
Aditya Gautam <adityagautam18@gmail.com>
2022-05-24 10:35:43 +0530

    Add files via upload

---
+++ stores/configurations/stableSwapRouteAssets.js
@@ -0,0 +1,8 @@
+// list of assets we support for providing liquidity to the factory. We will have to expand on this.
+export default [{
+  address: '0xb8eBD5E70e8f8a1fc2978Ea32e9866E1deF71a52',
+  name: 'MIM',
+  symbol: 'MIM',
+  decimals: 18,
+  logo: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3/logo.png'
+}]
