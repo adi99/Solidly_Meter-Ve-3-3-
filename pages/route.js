053964354dd2d8e73b0d62d6c97b9a314952d048
Aditya Gautam <adityagautam18@gmail.com>
2022-05-24 10:43:09 +0530

    Add files via upload

---
+++ pages/route.js
@@ -0,0 +1,41 @@
+import Swap from "./swap";
+import Liquidity from "./liquidity";
+import LiquidityAddress from "./liquidity/[address]";
+import Vest from "./vest";
+import Vote from "./vote";
+import Rewards from "./rewards";
+import Whitelist from "./whitelist";
+import Bribe from "./bribe/create";
+
+import { useRouter } from "next/router";
+
+function Route({ changeTheme, ...props }) {
+  const router = useRouter();
+  const activePath = router.asPath;
+  if (activePath.includes("/swap")) {
+    return <Swap props={props} changeTheme={changeTheme} />;
+  } else if (activePath.includes("/liquidity")) {
+    if(activePath.includes("/liquidity/0x")) {
+      router.push(activePath)
+      return <LiquidityAddress props={props} changeTheme={changeTheme} />;
+    } else {
+      return <Liquidity props={props} changeTheme={changeTheme} />;
+    }
+  } else if (activePath.includes("/vest")) {
+    return <Vest props={props} changeTheme={changeTheme} />;
+  } else if (activePath.includes("/vote")) {
+    return <Vote props={props} changeTheme={changeTheme} />;
+  } else if (activePath.includes("/rewards")) {
+    return <Rewards props={props} changeTheme={changeTheme} />;
+  } else if (activePath.includes("/whitelist")) {
+    return <Whitelist props={props} changeTheme={changeTheme} />;
+  } else if (activePath.includes("/bribe")) {
+    return <Bribe props={props} changeTheme={changeTheme} />;
+  } else if (activePath.includes("/")) {
+    return <Swap props={props} changeTheme={changeTheme} />;
+  } else {
+    return <Swap props={props} changeTheme={changeTheme} />;
+  }
+}
+
+export default Route;
